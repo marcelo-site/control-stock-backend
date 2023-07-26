21a1d1ac6e93c4d760384724017edccfe380f0ac
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-07-26 12:34:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 // Solve cors
 const frontend = process.env.FRONTEND
 console.log(frontend)
-app.use(cors({ credentials: true, origin: 'control-stock-backend-production.up.railway.app'}))
+app.use(cors({ credentials: true, origin: 'https://control-stock-frotend-production.up.railway.app/'}))
 
 // Public folders for image
 app.use(express.static('public'))
